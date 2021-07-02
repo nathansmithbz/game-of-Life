7e9bf2f8978c0a52434a851564b6947d4f234d67
nathansmithbz <79979868+nathansmithbz@users.noreply.github.com>
2021-07-02 21:09:53 +0100

    Refactor to use objects for cells

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,76 +1,93 @@
-let cellSize = 5;
-let col, row, cells, cellsNext;
+let col, row;
 
 function setup() {
   createCanvas(500, 500);
-  frameRate(30);
-  //calculate num of rows/cols
+  cellSize = 5;
+  frameRate(10);
+  //create grid using 2d array to store state of cells
   col = floor(height / cellSize);
   row = floor(width / cellSize);
-  //create grid using 2 multidimmensional arrays to store state of cells
   cells = new Array(col);
-  //Create additional grid to store states for next itteration so we do not alter the current grid
-  cellsNext = new Array(col);
   for (let x = 0; x < col; x++) {
     cells[x] = new Array(row);
-    cellsNext[x] = new Array(row);
     for (let y = 0; y < col; y++) {
-      //Give cells a random starting state of dead or alive -> alive = 1, dead = 0
-      cells[x][y] = Math.floor(Math.random() * 2);
-      cellsNext[x][y] = 0;
+      cells[x][y] = new Cell(x, y, cellSize, Math.floor(Math.random() * 2));
     }
   }
 }
 
 function draw() {
-  //draw cells based on state of cell  -> alive = 1 (black), dead = 0 (white)
+  //Loop through the whole grid -> Draw cell, then check new cell status based on rules
   for (let x = 0; x < row; x++) {
     for (let y = 0; y < col; y++) {
-      if (cells[x][y] == 1) {
-        fill(0);
-      } else {
-        fill(255);
+      cells[x][y].display();
+      cells[x][y].update(cells);
+    }
+  }
+  //After every cell has been checked update all cells to their new status 
+  for (let x = 0; x < row; x++) {
+    for (let y = 0; y < col; y++) {
+      cells[x][y].nextItteration();
+    }
+  }
+}
+
+class Cell {
+  constructor(x, y, cellSize, state) {
+    this.x = x;
+    this.y = y;
+    this.size = cellSize;
+    this.state = state;
+    this.nextState = state;
+  }
+
+  //draw cells based on state of cell  -> alive = 1 (black), dead = 0 (white)
+  display() {
+    if (this.state == 1) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    rect(this.x * this.size, this.y * this.size, this.size, this.size);
+  }
+
+  //Sum number of alive neighbours 
+  sumNeighbours(arr) {
+    let neighbours = 0;
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        //Use Modulus -> left hand neighbours of cells on the far left of the grid are considered to be cells on the far right etc
+        neighbours += arr[(this.x + i + row) % row][(this.y + j + col) % col].state;
       }
-      rect(x * cellSize, y * cellSize, cellSize, cellSize);
-      checkRules(x, y);
+    }
+    //remove current cell status before returning as it was included in the previous loop 
+    return (neighbours -= this.state);
+  }
+  
+  update(arr) {
+    let temp = this.sumNeighbours(arr);
+    if (this.state == 1) {
+      //When a live cell has fewer than two neighbours, then this cell dies
+      if (temp < 2) {
+        this.nextState = 0;
+      }
+      //When a live cell has more than three neighbours, then this cell dies
+      else if (temp > 3) {
+        this.nextState = 0;
+      } 
+      //When a live cell has two or three neighbours, then this cell stays alive
+      else {
+        this.nextState = 1;
+      }
+    }
+    //When an empty position has exactly three neighbouring cells, then a cell is created in this position
+    if (this.state == 0) {
+      if (temp == 3) {
+        this.nextState = 1;
+      }
     }
   }
-  //Now safe to create a deep copy of the next iteration array, to be displayed on the next draw() call
-  cells = JSON.parse(JSON.stringify(cellsNext));
-}
-
-function sumNeighbours(x, y) {
-  //Sum number of alive neighbours -
-  let neighbours = 0;
-  for (let i = -1; i < 2; i++) {
-    for (let j = -1; j < 2; j++) {
-      neighbours += cells[(x + i + row) % row][(y + j + col) % col];
-    }
-  }
-  //Return sum - current cell status
-  return (neighbours -= cells[x][y]);
-}
-
-function checkRules(x, y) {
-  let temp = sumNeighbours(x, y);
-  if (cells[x][y] == 1) {
-    //When a live cell has fewer than two neighbours, then this cell dies
-    if (temp < 2) {
-      cellsNext[x][y] = 0;
-    }
-    //When a live cell has more than three neighbours, then this cell dies
-    else if (temp > 3) {
-      cellsNext[x][y] = 0;
-    }
-    //When a live cell has two or three neighbours, then this cell stays alive
-    else {
-      cellsNext[x][y] = 1;
-    }
-  }
-  //When an empty position has exactly three neighbouring cells, then a cell is created in this position
-  if (cells[x][y] == 0) {
-    if (temp == 3) {
-      cellsNext[x][y] = 1;
-    }
+  nextItteration() {
+    this.state = this.nextState;
   }
 }
